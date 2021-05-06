6e12b8e250b345a59c97a8ccf5b7bbbe44fad7d0
hermes7308 <hermes7308@gmail.com>
2021-05-06 19:25:43 -0400

    update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ PID_FILE=zoom_clone.pid
 if test -f "$PID_FILE"; then
     echo "$PID_FILE exists. Please check the kraken bot process is started."
 else
-    nohup npm start > /home/irteam/logs/zoom-clone/zoom-clone.log 2>&1 &
+    nohup node server.js > /home/irteam/logs/zoom-clone/zoom-clone.log 2>&1 &
     echo $! >$PID_FILE
 fi
 
